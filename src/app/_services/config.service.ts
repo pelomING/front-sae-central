c84caedba1d3895d99029d265ca426f93e1ec657
Patracom <pelomingenieria2024@gmail.com>
2023-11-02 16:06:33 -0300

    actualzacion en sistema sae turnos

--- src/app/_services/config.service.ts
+++ src/app/_services/config.service.ts
@@ -6,25 +6,10 @@ import { environment } from '../../environments/environment';
 })
 export class ConfigService {
     
-
     baseUrl: string = 'https://backend-sae-postgres-production.up.railway.app';
 
     //baseUrl: string = 'https://backend-pelom-desarrollo.up.railway.app';
 
     //baseUrl: string = 'http://localhost:8080';
- 
 
-    constructor() {
-
-        // if (environment.baseUrl) {
-        //     this.baseUrl = environment.baseUrl;
-        // } else {
-        //     this.baseUrl = process.env['URL_API']; 
-        // }
-
-    }
-
-    setBaseUrl(url: string): void {
-        this.baseUrl = url;
-    }
 }
