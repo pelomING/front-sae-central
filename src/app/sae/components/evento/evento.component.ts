9688d91cda9eff9d9b03315c2288695f4c105d9e
Patracom <pelomingenieria2024@gmail.com>
2023-11-14 11:15:05 -0300

    mejoras en modulos de eventos

--- src/app/sae/components/evento/evento.component.ts
+++ src/app/sae/components/evento/evento.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Eventos} from 'src/app/sae/model/eventos.model';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Table } from 'primeng/table';
+
+import { Eventos } from 'src/app/sae/model/eventos.model';
 import { EventoService } from 'src/app/sae/services/evento.service';
 
 @Component({
@@ -7,19 +11,59 @@ import { EventoService } from 'src/app/sae/services/evento.service';
   templateUrl: './evento.component.html'
 })
 export class EventoComponent implements OnInit {
+
+  cols: any[] = [];
   eventos?: Eventos[];
-  constructor(private eventoService: EventoService) { }
+
+  mostrarGuardar: boolean = true; // Mostrar el botón por defecto
+  mostrarActualizar: boolean = true;
+
+  formObraDialog: boolean;
+
+  constructor(
+    private eventoService: EventoService,
+    private fb: FormBuilder,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
+  ) { }
+
   ngOnInit(): void {
     this.recuperaEventos();
+
+
+    this.cols = [
+      { field: 'id', header: 'Id' },
+      { field: 'numero_ot', header: 'N° Ot' },
+      { field: 'tipo_evento', header: 'Tipo Evento' },
+      { field: 'rut_maestro', header: 'Maestro' },
+      { field: 'rut_ayudante', header: 'Ayudante' },
+      { field: 'turno', header: 'Turno' },
+      { field: 'paquete', header: 'Oficina' },
+      { field: 'requerimiento', header: 'Requerimiento' },
+      { field: 'direccion', header: 'Dirección' },
+      { field: 'fecha_hora', header: 'Fecha Hora Ejecución' }
+    ];
+
   }
 
   recuperaEventos(): void {
-    this.eventoService.getEventos().subscribe({next: (data) => {
-      
-      console.log("data eventos:",data);
-      
-      this.eventos = data 
+    this.eventoService.getEventos().subscribe({
+      next: (data) => {
 
-    }, error: (e) => console.error(e)});
+        console.log("data eventos:", data);
+
+        this.eventos = data
+
+      }, error: (e) => console.error(e)
+    });
   }
+
+
+
+  onGlobalFilter(table: Table, event: Event) {
+    table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+  }
+
+
+
 }
--- src/app/sae/components/jornada/jornada.component.ts
+++ src/app/sae/components/jornada/jornada.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Turnos} from 'src/app/sae/model/turnos.model';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Table } from 'primeng/table';
+
+import { Turnos } from 'src/app/sae/model/turnos.model';
 import { TurnosService } from 'src/app/sae/services/turnos.service';
 
 @Component({
@@ -7,13 +11,47 @@ import { TurnosService } from 'src/app/sae/services/turnos.service';
   templateUrl: './jornada.component.html'
 })
 export class JornadaComponent implements OnInit {
-  jornadas?: Turnos[];
-  constructor(private turnosService: TurnosService) { }
+
+  cols: any[] = [];
+
+  turnos?: Turnos[];
+
+  mostrarGuardar: boolean = true; // Mostrar el botón por defecto
+  mostrarActualizar: boolean = true;
+
+  formObraDialog: boolean;
+
+  constructor(private turnosService: TurnosService, 
+              private fb: FormBuilder,
+              private messageService: MessageService,
+              private confirmationService: ConfirmationService) { }
+
   ngOnInit(): void {
+
     this.recuperaJornadas();
+
+    this.cols = [
+      { field: 'id', header: 'Id' },
+      { field: 'rut_maestro', header: 'Maestro' },
+      { field: 'rut_ayudante', header: 'Ayudante' },
+      { field: 'turno', header: 'Turno' },
+      { field: 'patente', header: 'Patente' },
+      { field: 'km_inicial', header: 'Km inicial' },
+      { field: 'km_final', header: 'Km final' },
+      { field: 'fecha_hora_ini', header: 'Fecha Hora Inicio' },
+      { field: 'fecha_hora_fin', header: 'Fecha Hora Fin' }
+    ];
+
   }
 
   recuperaJornadas(): void {
-    this.turnosService.getJornada().subscribe({next: (data) => {this.jornadas = data}, error: (e) => console.error(e)});
+    this.turnosService.getJornada().subscribe({ next: (data) => { this.turnos = data }, error: (e) => console.error(e) });
   }
+
+
+  onGlobalFilter(table: Table, event: Event) {
+    table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+  }
+
+
 }
