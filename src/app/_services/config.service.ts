7cfaecb5f64c2210f7fdf15c65d38590802c1258
Patracom <pelomingenieria2024@gmail.com>
2023-12-27 17:52:03 -0300

    modifico fechas en estado resultado y agrego algo en config.service validacion por url

--- src/app/_services/config.service.ts
+++ src/app/_services/config.service.ts
@@ -6,12 +6,41 @@ import { environment } from '../../environments/environment';
 })
 
 export class ConfigService {
-    
+
     //conectar a produccion backend
-    baseUrl: string = 'https://backend-pelom-production.up.railway.app';
- 
+    //baseUrl: string = 'https://backend-pelom-production.up.railway.app';
     //baseUrl: string = 'https://backend-pelom-desarrollo.up.railway.app';
-     
     //baseUrl: string = 'http://localhost:8080';
- 
+
+    baseUrl: string;
+
+    constructor() {
+
+        this.setBaseUrl();
+
+    }
+
+
+    private setBaseUrl(): void {
+
+        const currentUrl = window.location.origin;
+    
+        if (currentUrl === 'https://pelom-ing.up.railway.app') {
+
+          this.baseUrl = 'https://backend-pelom-production.up.railway.app';
+
+        } else if (currentUrl === 'https://pelom-ing-dev.up.railway.app') {
+
+          this.baseUrl = 'https://backend-pelom-desarrollo.up.railway.app';
+
+        } else {
+
+          // Por defecto, utiliza localhost si no coincide con las URL conocidas
+          this.baseUrl = 'http://localhost:8080';
+
+        }
+
+      }
+
+
 }
